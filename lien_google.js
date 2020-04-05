e9d7a1be4462001be5bc749db04ed437d7e6f7bb
Olivier Ross <olivier.ross@usherbrooke.ca>
2020-04-05 19:50:19 -0400

    liens

--- lien_google.js
+++ lien_google.js
@@ -1,4 +1,7 @@
-let email_regexp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/g;
+let email_regexp = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+let phone_regexp = /(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}/
+let url_regexp = /(?:^|[^@\.\w-])([a-z0-9]+:\/\/)?(\w(?!ailto:)\w+:\w+@)?([\w.-]+\.[a-z]{2,4})(:[0-9]+)?(\/.*)?(?=$|[^@\.\w-])/i;
+//let url_regexp = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/
 
 window.onload = async function () {
   let response = await fetch("https://sheets.googleapis.com/v4/spreadsheets/1inmEtfz_sZeA1bI6n2y581WnwOm44B-KTAxhacw2vJY/values/'IGNORER - Données publiques'!A:H?key=AIzaSyAl3TfynOtVS2PQRKyJPWxJShQdESCvsy4");
@@ -23,7 +26,7 @@ function ajouter_donnees_DOM (json) {
   });
 
   // Creer entete
-  /*let thead = document.createElement("thead");
+  let thead = document.createElement("thead");
 
   _.each(json.values.slice(0, 1), (colonne) => {
     let tr = document.createElement("tr");
@@ -34,7 +37,7 @@ function ajouter_donnees_DOM (json) {
     });
     thead.appendChild(tr);
   });
-  
+
   // Creer corps de la table
   let tbody = document.createElement("tbody");
   _.each(organismes, (organisme) => {
@@ -42,10 +45,21 @@ function ajouter_donnees_DOM (json) {
     _.each(organisme, (val) => {
       let td = document.createElement("td");
 
-      // TODO creer des liens cliquables
-      if(val.match(email_regexp)) {
-        console.log("email!");
-      }
+      // Sites
+      val = val.replace(url_regexp, function (match) {
+        return "<a href='" + match + "'>" + match + "</a>";
+      });
+
+      // Telephones
+      val = val.replace(phone_regexp, function (match) {
+        return "<a href='tel:" + match + "'>" + match + "</a>";
+      });
+
+      // Courriels
+      val = val.replace(email_regexp, function (match) {
+        return "<a href='mailto:" + match + "'>" + match + "</a>";
+      });
+
       td.innerHTML = val;
       tr.appendChild(td);
     });
@@ -53,15 +67,15 @@ function ajouter_donnees_DOM (json) {
   });
 
   table.appendChild(thead);
-  table.appendChild(tbody);*/
+  table.appendChild(tbody);
 
   // Initialisation de Datatables
   let groupColumn = 0;
-  
+
   $('#table').DataTable({
     responsive: true,
-    data: organismes,
-    columns: colonnes,
+    //data: organismes,
+    //columns: colonnes,
     paging: false,
     "columnDefs": [
       {
