d1f3c83e3cc0e6101d6f02781b6f80d25f6410bf
Olivier Ross <olivier.ross@usherbrooke.ca>
2020-04-27 23:05:31 -0400

    icones

--- lien_google.js
+++ lien_google.js
@@ -1,7 +1,6 @@
 let email_regexp = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 let phone_regexp = /(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}/
 let url_regexp = /(?:^|[^@\.\w-])([a-z0-9]+:\/\/)?(\w(?!ailto:)\w+:\w+@)?([\w.-]+\.[a-z]{2,4})(:[0-9]+)?(\/.*)?(?=$|[^@\.\w-])/i;
-//let url_regexp = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/
 
 window.onload = async function () {
   let response = await fetch("https://sheets.googleapis.com/v4/spreadsheets/1inmEtfz_sZeA1bI6n2y581WnwOm44B-KTAxhacw2vJY/values/'IGNORER - Données publiques'!A:H?key=AIzaSyAl3TfynOtVS2PQRKyJPWxJShQdESCvsy4");
@@ -14,9 +13,9 @@ window.onload = async function () {
 }
 
 function ajouter_donnees_DOM (json) {
+  let colonnes = json.values.slice(0, 1)[0].map((col) => { return {title: col} }).slice(0, 5);
   let organismes = json.values.slice(1);
-  let colonnes = json.values.slice(0, 1)[0].map((col) => { return {title: col} });
-  let table = document.getElementById("table");
+  console.log(colonnes);
 
   // Ajustement des types de donnees
   _.each(organismes, (organisme) => {
@@ -25,121 +24,78 @@ function ajouter_donnees_DOM (json) {
     }
   });
 
-  // Creer entete
-  let thead = document.createElement("thead");
+  // Regroupement des donnees
+  let categories = _.groupBy(organismes, (organisme) => { return organisme[0]; });
 
-  _.each(json.values.slice(0, 1), (colonne) => {
+  // Pour chaque section
+  _.each(categories, (categorie, key) => {
+
+    // Creer table
+    let table = document.createElement("table");
+    //table.id = 'table' + key;
+
+    // Creer entete
+    let entete = document.createElement("h3");
+    entete.innerHTML = key;
+
+    let thead = document.createElement("thead");
     let tr = document.createElement("tr");
-    _.each(colonne, (val) => {
+
+    _.each(colonnes, (val) => {
       let td = document.createElement("td");
-      td.innerHTML = val;
+      td.innerHTML = val.title;
       tr.appendChild(td);
     });
     thead.appendChild(tr);
-  });
 
-  // Creer corps de la table
-  let tbody = document.createElement("tbody");
-  _.each(organismes, (organisme) => {
-    let tr = document.createElement("tr");
-    _.each(organisme, (val) => {
-      let td = document.createElement("td");
+    // Creer corps de la table
+    let tbody = document.createElement("tbody");
+    _.each(categorie, (organisme) => {
+      let tr = document.createElement("tr");
+      _.each(organisme, (val) => {
 
-      // Sites
-      val = val.replace(url_regexp, function (match) {
-        return "<a href='" + match + "'>" + match + "</a>";
-      });
-
-      // Telephones
-      val = val.replace(phone_regexp, function (match) {
-        return "<a href='tel:" + match + "'>" + match + "</a>";
-      });
-
-      // Courriels
-      val = val.replace(email_regexp, function (match) {
-        return "<a href='mailto:" + match + "'>" + match + "</a>";
-      });
-
-      td.innerHTML = val;
-      tr.appendChild(td);
-    });
-    tbody.appendChild(tr);
-  });
-
-  table.appendChild(thead);
-  table.appendChild(tbody);
-
-  // Initialisation de Datatables
-  let groupColumn = 0;
-
-  $('#table').DataTable({
-    responsive: true,
-    //data: organismes,
-    //columns: colonnes,
-    paging: false,
-    "columnDefs": [
-      {
-        "targets": [0],
-        "visible": false,
-        "searchable": false
-      },
-      {
-        "targets": [1],
-        responsivePriority: 1
-      },
-      {
-        "targets": [2],
-        responsivePriority: 2
-      }
-    ],
-    "order": [[ groupColumn, 'asc'  ]],
-    "drawCallback": function ( settings  ) {
-      var api = this.api();
-      var rows = api.rows( {page:'current'}  ).nodes();
-      var last=null;
-
-      api.column(groupColumn, {page:'current'} ).data().each( function ( group, i  ) {
-        if ( last !== group  ) {
-          $(rows).eq( i  ).before(
-            '<tr class="group"><td colspan="' + (colonnes.length - 2) + '">'+group+'</td></tr>'
-          );
-          last = group;
+        let courriel, site, tel, fb;
+        
+        // Courriels
+        if(courriel = val.match(email_regexp)) {
+          let a = document.createElement("a");
+          a.href = "mailto:" + courriel;
+          a.innerHTML = '<img class="icone" src="courriel.png" alt="courriel"/>';
+          tr.childNodes[1].appendChild(a);
+        }
+        // Telephones
+        else if (tel = val.match(phone_regexp)) {
+          let a = document.createElement("a");
+          a.href = "tel:" + tel;
+          a.innerHTML = '<img class="icone" src="tel.png" alt="telephone"/>';
+          tr.childNodes[1].appendChild(a);
+        }
+        // Sites
+        else if (site = val.match(url_regexp)) {
+          let a = document.createElement("a");
+          a.href = site;
+          if(val.match(/facebook/i)) {
+            a.innerHTML = '<img class="icone" src="fb.png" alt="facebook"/>';
+          }
+          else {
+            a.innerHTML = '<img class="icone" src="web.png" alt="site web"/>';
+          }
+          tr.childNodes[1].appendChild(a);
+        }
+        // Contenu
+        else {
+          let td = document.createElement("td");
+          td.innerHTML = val;
+          tr.appendChild(td);
         }
       });
-    },
-    language: donnees_fr
+      tbody.appendChild(tr);
+    });
+
+    table.appendChild(thead);
+    table.appendChild(tbody);
+
+    document.getElementById("section-table").appendChild(entete);
+    document.getElementById("section-table").appendChild(table);
   });
-}
-
-///// DONNEES /////
-
-var donnees_fr = {
-  "sEmptyTable":     "Aucune donnée disponible dans le tableau",
-  "sInfo":           "Affichage de l'élément _START_ à _END_ sur _TOTAL_ éléments",
-  "sInfoEmpty":      "Affichage de l'élément 0 à 0 sur 0 élément",
-  "sInfoFiltered":   "(filtré à partir de _MAX_ éléments au total)",
-  "sInfoPostFix":    "",
-  "sInfoThousands":  ",",
-  "sLengthMenu":     "Afficher _MENU_ éléments",
-  "sLoadingRecords": "Chargement...",
-  "sProcessing":     "Traitement...",
-  "sSearch":         "Rechercher :",
-  "sZeroRecords":    "Aucun élément correspondant trouvé",
-  "oPaginate": {
-    "sFirst":    "Premier",
-    "sLast":     "Dernier",
-    "sNext":     "Suivant",
-    "sPrevious": "Précédent"
-  },
-  "oAria": {
-    "sSortAscending":  ": activer pour trier la colonne par ordre croissant",
-    "sSortDescending": ": activer pour trier la colonne par ordre décroissant"
-  },
-  "select": {
-    "rows": {
-      "_": "%d lignes sélectionnées",
-      "0": "Aucune ligne sélectionnée",
-      "1": "1 ligne sélectionnée"
-    }
-  }
 }
