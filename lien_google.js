a722af6e29e5d3726f0d0c51bf6d106b07906e16
Olivier Ross <olivier.ross@usherbrooke.ca>
2020-06-02 09:30:47 -0400

    nouveaux noms

--- lien_google.js
+++ lien_google.js
@@ -139,11 +139,13 @@ function ajouter_donnees_DOM (json) {
       // Deuxieme case
       let etat = document.createElement("td");
       etat.innerHTML = organisme._tat;
+      // TODO etat.innerHTML = organisme.locaux_accessibles_;
       tr.appendChild(etat);
 
       // Troisieme case
       let reduits = document.createElement("td");
       reduits.innerHTML = organisme.services_r_duits;
+      // TODO reduits.innerHTML = organisme.services_actuels;
       tr.appendChild(reduits);
 
       // Quatrieme case
